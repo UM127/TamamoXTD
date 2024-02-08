f2c8e672c7954a7a32d1b2096891a3ce90db8847
kbc21b03 <kbc21b03@stu.kawahara.ac.jp>
2024-02-08 13:32:48 +0900

    update

--- GameTemplate/Game/GameCamera.cpp
+++ GameTemplate/Game/GameCamera.cpp
@@ -7,7 +7,7 @@ bool GameCamera::Start()
 {
 	//注視点から視点までのベクトルを設定。
 	//m_toCameraPos.Set(0.0f, 150.0f, 800.0f);
-	m_toCameraPos.Set(0.0f, 70.0f, 200.0f);
+	m_toCameraPos.Set(0.0f, 0.0f, 100.0f);
 	//プレイヤーのインスタンスを探す。
 	m_player = FindGO<Player>("player");
 
